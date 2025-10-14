9f6f451908f0c20ec0ea043f2c1a760f373f0003
bitaniyadamtew <bitaniyadamtew@gmail.com>
2025-10-14 23:13:51 +0300

    feat:"implemented UserService class"

---
+++ user.service.js
@@ -0,0 +1,21 @@
+const UserRepo=require('../repository/user.repository.js')
+
+class UserService{
+    async addUser(userData){
+        return await UserRepo.createUser(userData)
+    }
+    async getUsers(){
+        return await UserRepo.getUsers()
+    }
+    async getUserById(userId){
+        return await UserRepo.getUserById(userId)
+    }
+    async updateUser(userId,updatedData){
+        return await UserRepo.updateUser(userId,updatedData)
+    }
+    async deleteUser(userId){
+        return await UserRepo.deleteUser(userId)
+    }
+
+}
+module.exports=new UserService()
