6d3b37a8443e2d09472fbd07531e6afcf637f481
Bitaniya <bitaniyadamtew@gmail.com>
2025-10-16 15:55:36 +0300

    feat: add routes for update product

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,7 +4,10 @@ const router=express.Router();
 
 const productControllers = require('../controllers/product.controllers');
 
-router.post('/v1/products/:id',productControllers.addProduct)
-router.put('/v1/products',productControllers.updateProduct)
-router.get('/v1/products',productControllers.getProducts)
-router.get('/v1/products/:id', productControllers.getProductbyId)
+router.post('/v1/Products',(req,res)=>productControllers.addProduct(req,res))
+router.put('/v1/Products/:id',(req,res)=>productControllers.updateProduct(req,res))
+router.get('/v1/Products',(req,res)=>productControllers.getProducts(req,res))
+router.get('/v1/Products/:id',(req,res)=>productControllers.getProductbyId(req,res))
+router.delete('/v1/Products/:id',(req,res)=>productControllers.deleteProduct(req,res))
+
+module.exports=router
